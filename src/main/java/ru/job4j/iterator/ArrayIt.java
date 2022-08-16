b0f389efb5cdc7248d29922375f35f9dd5028c88
ДиЕ <denezhkin.dda@gmail.com>
2022-08-16 18:58:52 +0300

    Подключение библиотеки AssertJ [#504881]

--- src/main/java/ru/job4j/iterator/ArrayIt.java
+++ src/main/java/ru/job4j/iterator/ArrayIt.java
@@ -7,7 +7,6 @@ import java.util.NoSuchElementException;
 public class ArrayIt implements Iterator<Integer> {
     private final int[] data;
     private int point = 0;
-
     public ArrayIt(int[] data) {
         this.data = data;
     }
--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -1,20 +1,20 @@
 package ru.job4j.iterator;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArrayItTest {
     @Test
-    public void whenMultiCallhasNextThenTrue() {
+    public void whenMultiCallHasNextThenTrue() {
         ArrayIt it = new ArrayIt(
                 new int[] {1, 2, 3}
         );
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
+        boolean rsl = it.hasNext();
+        assertThat(rsl).isTrue();
+        assertThat(it.hasNext()).isTrue();
     }
 
     @Test
@@ -22,16 +22,8 @@ public class ArrayItTest {
         ArrayIt it = new ArrayIt(
                 new int[] {1, 2, 3}
         );
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-        assertThat(it.next(), is(3));
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void whenNextFromEmpty() {
-        ArrayIt it = new ArrayIt(
-                new int[] {}
-        );
-        it.next();
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.next()).isEqualTo(3);
     }
 }
