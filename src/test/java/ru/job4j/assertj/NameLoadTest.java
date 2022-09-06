52f0aafb3ae3d760fe5e5c69e4ce3ec6f98c7d2f
ДиЕ <denezhkin.dda@gmail.com>
2022-09-06 20:27:34 +0300

    Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.assertj;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NameLoadTest {
+
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParse() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void checkValidate() {
+        NameLoad nameLoad = new NameLoad();
+        String word = "name";
+        assertThatThrownBy(() -> nameLoad.parse(word))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(word)
+                .hasMessageContaining("does not contain the symbol \"=\"");
+    }
+
+    @Test
+    void checkValidateSecond() {
+        NameLoad nameLoad = new NameLoad();
+        String word = "=name";
+        assertThatThrownBy(() -> nameLoad.parse(word))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(word)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void checkValidateThird() {
+        NameLoad nameLoad = new NameLoad();
+        String word = "name=";
+        assertThatThrownBy(() -> nameLoad.parse(word))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(word)
+                .hasMessageContaining("does not contain a value");
+    }
+}
