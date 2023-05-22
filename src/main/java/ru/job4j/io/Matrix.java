f98c0c8b04a57df840f9e3f4121a89e716f39523
ДиЕ <denezhkin.dda@gmail.com>
2023-05-22 23:30:56 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Matrix {
+    public static void multiple(int size) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                out.write((j == 0 ? "" : "\t").getBytes());
+                out.write(Integer.toString((i + 1) * (j + 1)).getBytes());
+        }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        multiple(9);
+    }
+}
