e68a899753ada6d4157fade2da2f05fa567a2a35
ДиЕ <denezhkin.dda@gmail.com>
2023-05-26 18:17:11 +0300

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,12 +1,10 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public class LogFilter {
-    public List<String> filter(String file) {
+    public static List<String> filter(String file) {
         List<String> list = null;
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             list = in.lines().filter(s -> s.contains(" 404 ")).toList();
@@ -16,10 +14,21 @@ public class LogFilter {
         return list;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String i : log) {
+                out.printf(i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter();
-        List<String> log = logFilter.filter("data/log.txt");
-        log.forEach(System.out::println);
+        List<String> log = filter("data/log.txt");
+        save(log, "data/404.txt");
     }
 }
-
