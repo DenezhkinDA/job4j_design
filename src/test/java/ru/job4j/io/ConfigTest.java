0b633091e0e9b5f917298e195bf3b1bc842eddd8
ДиЕ <denezhkin.dda@gmail.com>
2023-05-29 20:02:20 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username")).isEqualTo("postgres");
+    }
+
+    @Test
+    void whenWithoutKey() {
+        String path = "./data/without_key.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenWithoutValue() {
+        String path = "./data/without_value.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenWithoutEqual() {
+        String path = "./data/without_equal.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenWithoutKeyAndValue() {
+        String path = "./data/without_key_and_value.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenEmptyLines() {
+        String path = "./data/with_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+        assertThat(config.value("hibernate.connection.url")).isEqualTo("jdbc:postgresql://127.0.0.1:5432/trackstudio");
+        assertThat(config.value("hibernate.connection.username")).isEqualTo("postgres");
+    }
+}
