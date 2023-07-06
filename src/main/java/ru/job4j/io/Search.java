89106231f013cd04f3be612a350537bdf17e47ca
ДиЕ <denezhkin.dda@gmail.com>
2023-07-06 20:57:52 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        check(args);
+        Path start = Paths.get(args[2]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static void check(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("You must specify 2 arguments.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Folder not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+
+        if (!(args[1].length() > 1) && !(args[1].startsWith("."))) {
+            throw new IllegalArgumentException(String.format("%s - not an extension of the file", args[1]));
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
